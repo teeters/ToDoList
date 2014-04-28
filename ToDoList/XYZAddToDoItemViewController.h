d0bd6fb88bd742ca5a0d4d7b7456bef9a67138f5
teeters <samteeter93@yahoo.com>
2014-04-28 13:57:54 +0100

    Initial commit

---
+++ ToDoList/XYZAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZAddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by Samuel Teeter on 4/27/14.
+//  Copyright (c) 2014 Samuel Teeter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/XYZAddToDoItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  XYZAddToDoItemViewController.m
+//  ToDoList
+//
+//  Created by Samuel Teeter on 4/27/14.
+//  Copyright (c) 2014 Samuel Teeter. All rights reserved.
+//
+
+#import "XYZAddToDoItemViewController.h"
+
+@interface XYZAddToDoItemViewController ()
+
+@end
+
+@implementation XYZAddToDoItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
